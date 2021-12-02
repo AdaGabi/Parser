b34f3e02cedefd819d159d937d1a2a5a4e436c4c
Ada Birtocian <adabirtocian@yahoo.com>
2021-12-02 18:41:04 +0200

    use epsilon for empty sequence; compute follow only when needed

--- Parser.py
+++ Parser.py
@@ -6,8 +6,10 @@ class Parser:
         self.__first_set = dict()
         self.__follow_set = dict()
         self.__grammar: Grammar = grammar
+        self.__need_follow = False
         self.__compute_first_set()
-        self.__compute_follow_set()
+        if self.__need_follow:
+            self.__compute_follow_set()
 
     def __compute_first_set(self):
         for non_terminal in self.__grammar.get_non_terminals():
@@ -18,15 +20,17 @@ class Parser:
             return self.__first_set[non_terminal]
 
         first = set()
-        for production, _ in self.__grammar.get_productions_non_terminal(non_terminal):
-            first_symbol = production.split(' ')[0]
-            if first_symbol == 'Є':
-                first.add(first_symbol)
-            elif first_symbol in self.__grammar.get_terminals():
-                first.add(first_symbol)
-            else:
-                for symbol in self.first(first_symbol):
-                    first.add(symbol)
+        if non_terminal != "epsilon":
+            for production, _ in self.__grammar.get_productions_non_terminal(non_terminal):
+                first_symbol = production.split(' ')[0]
+                if first_symbol == "epsilon":
+                    first.add(first_symbol)
+                    self.__need_follow = True
+                elif first_symbol in self.__grammar.get_terminals():
+                    first.add(first_symbol)
+                else:
+                    for symbol in self.first(first_symbol):
+                        first.add(symbol)
 
         return first
 
@@ -40,7 +44,8 @@ class Parser:
 
         follow = set()
         if non_terminal == self.__grammar.get_start_symbol():
-            follow.add('Є') 
+            follow.add("epsilon")
+            return follow
 
         for nt in self.__grammar.get_productions():
             for production, _ in self.__grammar.get_productions_non_terminal(nt):
@@ -48,28 +53,42 @@ class Parser:
                     continue
 
                 symbols: list = production.split(' ')
-                
+
                 if symbols.index(non_terminal) < len(symbols) - 1:
                     follow_symbol = symbols[symbols.index(non_terminal) + 1]
-                    if follow_symbol == 'Є':
+                    if follow_symbol == "epsilon":
                         follow = follow.union(follow(nt))
                     elif follow_symbol in self.__grammar.get_terminals():
                         follow.add(follow_symbol)
                     else:
                         first_set: set = self.first(follow_symbol)
-                        if 'Є' in first_set:
-                            first_set.remove('Є')
+                        if "epsilon" in first_set:
+                            first_set.remove("epsilon")
                             follow = follow.union(self.follow(nt))
                         follow = follow.union(first_set)
                 else:
-                    follow = follow.union(self.follow(nt))
-                
+                    # follow = follow.union(self.follow(nt))
+                    follow.add("epsilon")
+
         return follow
+
+    def get_first_string(self):
+        first_string = ""
+        for key in self.__first_set:
+            first_string += key + ": " + str(self.__first_set[key]) + "\n"
+
+        return first_string
+
+    def get_follow_string(self):
+        follow_string = ""
+        for key in self.__follow_set:
+            follow_string += key + ": " + str(self.__follow_set[key]) + "\n"
+
+        return follow_string
 
 
 g = Grammar("g1.txt")
 p = Parser(g)
-# print(p.first("S"))
-print('Foolo')
-# print(p.follow('B'))
-
+print(p.get_first_string())
+print("Follow")
+print(p.get_follow_string())

58b3f42680eaff0a3509482bc95680111eaaab60
Ada Birtocian <adabirtocian@yahoo.com>
2021-12-10 10:12:09 +0200

    code refactoring for parsing algo

--- Parser.py
+++ Parser.py
@@ -9,6 +9,7 @@ class Parser:
         self.__grammar: Grammar = grammar
         self.__need_follow = False
         self.__ll1_table = dict()
+        self.__parsing_tree = list()
 
         self.__compute_first()
         if self.__need_follow:
@@ -219,55 +220,58 @@ class Parser:
 
                 elif ll1_entry[0] == 'error':
                     print("Error at: ", input_elem, working_elem)
-                    return None
+                    return
 
                 elif ll1_entry[0] == 'accept':
                     print("Sequence accepted")
-                    return output_stack
+                    self.tree(output_stack)
+                    return
 
                 else:
                     working_stack.pop(0)  # eliminate symbol from working stack
                     rhs_production = ll1_entry[0].split()
                     working_stack = rhs_production + working_stack  # add the new symbols
                     output_stack += [ll1_entry[1]]  # add the production number
-                    
-        return output_stack
 
     def tree(self, output_stack):
-        tree = list()
-        tree.append({"index": 1, "info": self.__grammar.get_start_symbol(), "parent": 0, "left": 0})
+        self.__parsing_tree = list()
+        self.__parsing_tree.append({"index": 1, "info": self.__grammar.get_start_symbol(), "parent": 0, "left": 0})
 
-        return self.__tree_rec(output_stack, 1, tree)
+        self.__tree_rec(output_stack, 1)
 
-    def __tree_rec(self, output_stack, parent_index, tree):
-        index = len(tree) + 1
+    def __tree_rec(self, output_stack, parent_index):
+        index = len(self.__parsing_tree) + 1
         prod_index = output_stack.pop(0)
         production_symbols = self.__grammar.get_production_by_index(prod_index)[1].split(' ')
         left = 0
         indexes = []
         for s in production_symbols:
-            tree.append({"index": index, "info": s, "parent": parent_index, "left": left})
+            self.__parsing_tree.append({"index": index, "info": s, "parent": parent_index, "left": left})
             indexes.append(index)
             left = index
             index += 1
 
         for i in range(len(production_symbols)):
             if production_symbols[i] in self.__grammar.get_non_terminals():
-                self.__tree_rec(output_stack, indexes[i], tree)
+                self.__tree_rec(output_stack, indexes[i])
 
         if len(output_stack) == 0:
-            return tree
+            return
+
+    def print_parsing_tree(self):
+        for line in self.__parsing_tree:
+            print("index: ", line["index"], "info: ", line["info"], "parent: ", line["parent"], "left: ", line["left"])
 
 
-g = Grammar("g1.txt")
+g = Grammar("g2.txt")
 p = Parser(g)
 # print(p.get_first_string())
 # print("Follow")
 # print(p.get_follow_string())
 # p.print_ll1_table()
-# stack = p.parsing_algo("go { id eq const ; }")
-# print(stack)
+stack = p.parsing_algo("go { number array [ const ] id ; }")
+p.print_parsing_tree()
 # print(g.get_productions())
-output = p.parsing_algo("d a d a")
-print(output)
-print(p.tree(output))
+# output = p.parsing_algo("d a d a")
+# print(output)
+# print(p.tree(output))
